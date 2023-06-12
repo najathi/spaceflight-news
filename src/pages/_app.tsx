c0eada0f3923bb29cfa5dbf2a57280e5928d3b55
najathi <najathi777@gmail.com>
2023-06-13 00:38:37 +0530

    fix redux presist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,26 @@
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import '../styles/globals.css'
 
-import { wrapper } from '../store';
+import { persistor, store, wrapper } from '../store';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Layout>
-            <Toaster
-                position="top-center"
-                reverseOrder={false}
-            />
-            <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Layout>
+                    <Toaster
+                        position="top-center"
+                        reverseOrder={false}
+                    />
+                    <Component {...pageProps} />
+                </Layout>
+            </PersistGate>
+        </Provider >
     );
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,31 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import counterReducer from './slice/counterSlice';
 import favoritesReducer from './slice/favoritesSlice';
 
-export const rootReducer = combineReducers({
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({
     counter: counterReducer,
     favorites: favoritesReducer,
 });
 
-export const wrapper = createWrapper(
-    () => configureStore({ reducer: rootReducer }),
-    { debug: true }
-);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+
+const makeStore = () => {
+    return store;
+}
+
+export const wrapper = createWrapper(makeStore);
