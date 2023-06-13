8367b32d6a891efa7760fe5909f2d860d9be23ad
najathi <najathi777@gmail.com>
2023-06-13 08:40:38 +0530

    nextnprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppProps } from 'next/app';
-import Layout from '../components/Layout';
 import { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import NextNProgress from 'nextjs-progressbar';
 
 import '../styles/globals.css'
 
+import Layout from '../components/Layout';
 import { persistor, store, wrapper } from '../store';
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -13,6 +14,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                 <Layout>
+                    <NextNProgress
+                        color="#29D"
+                        height={2}
+                        showOnShallow={true}
+                    />
                     <Toaster
                         position="top-center"
                         reverseOrder={false}
